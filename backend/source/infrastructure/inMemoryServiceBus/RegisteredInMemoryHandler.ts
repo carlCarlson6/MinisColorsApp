9593cdebb8a94724a624b134eaa9887c82ab8596
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-09-24 20:38:12 +0200

    .

--- backend/source/infrastructure/inMemoryServiceBus/RegisteredInMemoryHandler.ts
+++ backend/source/infrastructure/inMemoryServiceBus/RegisteredInMemoryHandler.ts
@@ -1,6 +1,8 @@
+import { BusMessage } from "../../core/services/BusMessage";
+import { BusResponse } from "../../core/services/BusResponse";
 import { Handler } from "../../core/services/Handler";
 
-export class RegisteredInMemoryHandler<T,S> {
+export class RegisteredInMemoryHandler<T extends BusMessage, S extends BusResponse> {
     
     public get Name(): string {
         return this.name;
